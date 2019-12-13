7a641c7ddac2ed4a6736773b487243e1b6a59399
core23 <mail@core23.de>
2019-12-13 23:21:53 +0100

    Add missing type hints

--- src/DependencyInjection/Core23TwigFormatterExtension.php
+++ src/DependencyInjection/Core23TwigFormatterExtension.php
@@ -21,6 +21,9 @@ final class Core23TwigFormatterExtension extends Extension
         return 'core23_twig_formatter';
     }
 
+    /**
+     * @param array<mixed> $configs
+     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
